3fe9953d0df0848699bff080d81e20129a334987
Roger Anacleto <rogerdasilvacode7@gmail.com>
2025-11-27 23:09:18 -0300

    feat: routes login and signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { createBrowserRouter } from "react-router";
 import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
 
 const route = createBrowserRouter([
   {
     element: <Home/>,
     path: "/"
+  },
+  {
+    element: <Login/>,
+    path: "/login"
+  },
+  {
+    element: <Signup/>,
+    path: "/signup"
   }
 ])
 
