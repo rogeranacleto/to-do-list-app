0b2b5be742b7d2d1a61eb22b441755880d0f77b7
Roger Anacleto <rogerdasilvacode7@gmail.com>
2025-12-06 23:31:29 -0300

    add toast

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,35 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { route } from './App'
 import { RouterProvider } from 'react-router'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster
+      position="top-center"
+      reverseOrder={false}
+      gutter={8}
+      containerClassName=""
+      containerStyle={{}}
+      toasterId="default"
+      toastOptions={{
+        className: '',
+        duration: 5000,
+        removeDelay: 1000,
+        style: {
+          background: '#363636',
+          color: '#fff',
+        },
+
+        success: {
+          duration: 3000,
+          iconTheme: {
+            primary: 'green',
+            secondary: 'black',
+          },
+        },
+      }}
+    />
     <RouterProvider router={route}></RouterProvider>
   </StrictMode>,
 )
